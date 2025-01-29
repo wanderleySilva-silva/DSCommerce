f2de3af85236fa3271fc4dd7abdf432e014935dc
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-29 06:46:42 -0300

    refactor: Atributo email marcado como unique

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,10 +19,16 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
+	
 	private String phone;
+	
 	private LocalDate birthDate;
+	
 	private String password;
 
 	@OneToMany(mappedBy = "client")
