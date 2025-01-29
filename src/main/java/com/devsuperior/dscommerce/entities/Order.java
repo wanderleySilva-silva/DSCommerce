05519ab41f093b0db38ef571e71e813bcbdbe985
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-29 13:08:52 -0300

    feat: Adicionados equals e hashCode nas entidades

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -98,6 +99,23 @@ public class Order {
 
 	public List<Product> getProducts() {
 		return itens.stream().map(orderItem -> orderItem.getProduct()).toList();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
--- src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
@@ -1,9 +1,10 @@
 package com.devsuperior.dscommerce.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_order_item")
@@ -25,19 +26,19 @@ public class OrderItem {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -56,6 +57,23 @@ public class OrderItem {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
